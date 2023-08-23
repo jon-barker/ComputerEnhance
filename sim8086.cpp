c8e07abdb0517d07ba8ee4727863e6dedd012185
Jon Barker <jbarker@nvidia.com>
2023-08-23 17:18:44 -0600

    Code cleanup

--- sim8086.cpp
+++ sim8086.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <cstdint>
 
+constexpr int INSTRUCTION_SIZE = 2;
+constexpr int MOV_OPCODE = 34;
+
 struct Instruction
 {
     uint8_t opcode : 6;
@@ -13,8 +16,21 @@ struct Instruction
     uint8_t RM : 3;
 };
 
+std::string registerToString(int value, bool wide)
+{
+    static const char *registers[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+    static const char *wideRegisters[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+    return wide ? wideRegisters[value] : registers[value];
+}
+
 void decode(const std::vector<char> *buffer)
 {
+    if (buffer->size() < INSTRUCTION_SIZE)
+    {
+        std::cerr << "Invalid instruction size" << '\n';
+        return;
+    }
+
     Instruction inst;
     inst.opcode = ((*buffer)[0] & 0b11111100) >> 2;
     inst.D = ((*buffer)[0] & 0b00000010) >> 1;
@@ -23,7 +39,7 @@ void decode(const std::vector<char> *buffer)
     inst.REG = ((*buffer)[1] & 0b00111000) >> 3;
     inst.RM = (*buffer)[1] & 0b00000111;
 
-    if (static_cast<int>(inst.opcode) == 34)
+    if (static_cast<int>(inst.opcode) == MOV_OPCODE)
     {
         std::cout << "mov ";
     };
@@ -41,138 +57,14 @@ void decode(const std::vector<char> *buffer)
         dst = inst.RM;
     }
 
-    if (inst.W == 1)
-    {
-        switch (static_cast<int>(dst))
-        {
-        case 0:
-            std::cout << "ax";
-            break;
-        case 1:
-            std::cout << "cx";
-            break;
-        case 2:
-            std::cout << "dx";
-            break;
-        case 3:
-            std::cout << "bx";
-            break;
-        case 4:
-            std::cout << "sp";
-            break;
-        case 5:
-            std::cout << "bp";
-            break;
-        case 6:
-            std::cout << "si";
-            break;
-        case 7:
-            std::cout << "di";
-            break;
-        }
-    }
-    else
-    {
-        switch (static_cast<int>(dst))
-        {
-        case 0:
-            std::cout << "al";
-            break;
-        case 1:
-            std::cout << "cl";
-            break;
-        case 2:
-            std::cout << "dl";
-            break;
-        case 3:
-            std::cout << "bl";
-            break;
-        case 4:
-            std::cout << "ah";
-            break;
-        case 5:
-            std::cout << "ch";
-            break;
-        case 6:
-            std::cout << "dh";
-            break;
-        case 7:
-            std::cout << "bh";
-            break;
-        }
-    }
-
-    std::cout << ", ";
-
-    if (inst.W == 1)
-    {
-        switch (static_cast<int>(src))
-        {
-        case 0:
-            std::cout << "ax";
-            break;
-        case 1:
-            std::cout << "cx";
-            break;
-        case 2:
-            std::cout << "dx";
-            break;
-        case 3:
-            std::cout << "bx";
-            break;
-        case 4:
-            std::cout << "sp";
-            break;
-        case 5:
-            std::cout << "bp";
-            break;
-        case 6:
-            std::cout << "si";
-            break;
-        case 7:
-            std::cout << "di";
-            break;
-        }
-    }
-    else
-    {
-        switch (static_cast<int>(src))
-        {
-        case 0:
-            std::cout << "al";
-            break;
-        case 1:
-            std::cout << "cl";
-            break;
-        case 2:
-            std::cout << "dl";
-            break;
-        case 3:
-            std::cout << "bl";
-            break;
-        case 4:
-            std::cout << "ah";
-            break;
-        case 5:
-            std::cout << "ch";
-            break;
-        case 6:
-            std::cout << "dh";
-            break;
-        case 7:
-            std::cout << "bh";
-            break;
-        }
-    }
-
-    std::cout << '\n';
+    std::cout << registerToString(dst, inst.W) << ", " << registerToString(src, inst.W) << '\n';
 };
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << '\n';
         return 1;
     }
 
@@ -180,7 +72,7 @@ int main(int argc, char *argv[])
 
     if (!file)
     {
-        std::cerr << "Error opening file: " << argv[1] << std::endl;
+        std::cerr << "Error opening file: " << argv[1] << '\n';
         return 1;
     }
 
@@ -195,11 +87,11 @@ int main(int argc, char *argv[])
 
     file.close();
 
-    std::cout << "bits 16\n";
+    std::cout << "bits 16" << '\n';
     std::cout << '\n';
-    for (int i = 0; i < buffer.size(); i = i + 2)
+    for (int i = 0; i < buffer.size(); i = i + INSTRUCTION_SIZE)
     {
-        std::vector<char> instructionBuffer(buffer.begin() + i, buffer.begin() + i + 2);
+        std::vector<char> instructionBuffer(buffer.begin() + i, buffer.begin() + i + INSTRUCTION_SIZE);
         decode(&instructionBuffer);
     }
 
