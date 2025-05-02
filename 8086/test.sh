029258749d9d577627f2b6c9251d8506fb783e21
Jon Barker <jbarker@Jons-Laptop.local>
2025-05-01 23:05:16 -0600

    test script bugfix and add listing_0040

--- 8086/test.sh
+++ 8086/test.sh
@@ -1,14 +1,19 @@
 FNAME=listing_0037_single_register_mov
-nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}.asm
+nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}
 ./sim8086 ${FNAME} > ${FNAME}.asm
 diff <(grep -v '^;' ~/data/computer_enhance/perfaware/part1/${FNAME}.asm) <(grep -v '^;' ${FNAME}.asm)
 
 FNAME=listing_0038_many_register_mov
-nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}.asm
+nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}
 ./sim8086 ${FNAME} > ${FNAME}.asm
 diff <(grep -v '^;' ~/data/computer_enhance/perfaware/part1/${FNAME}.asm) <(grep -v '^;' ${FNAME}.asm)
 
 FNAME=listing_0039_more_movs
-nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}.asm
+nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}
+./sim8086 ${FNAME} > ${FNAME}.asm
+diff <(grep -v '^;' ~/data/computer_enhance/perfaware/part1/${FNAME}.asm) <(grep -v '^;' ${FNAME}.asm)
+
+FNAME=listing_0040_challenge_movs
+nasm ~/data/computer_enhance/perfaware/part1/${FNAME}.asm -o ${FNAME}
 ./sim8086 ${FNAME} > ${FNAME}.asm
 diff <(grep -v '^;' ~/data/computer_enhance/perfaware/part1/${FNAME}.asm) <(grep -v '^;' ${FNAME}.asm)
